8c72aa0d11e9b1e7640776882efe22396954ee94
MichaelDiers <md@sreid.de>
2022-09-26 22:34:53 +0200

    add sign up page

--- authentication-client/src/components/Password.tsx
+++ authentication-client/src/components/Password.tsx
@@ -1,8 +1,18 @@
-export function Password() {
+export function Password({ repeat = false }: { repeat?: boolean }) {
   return (
     <>
       <input className='form-element' type='password' id='password' name='password' required={true} placeholder=' '></input>
       <label className='form-element' htmlFor='password' >Password</label>
+      {
+        repeat
+          ? (
+            <>
+              <input className='form-element' type='password' id='passwordRepeat' name='passwordRepeat' required={true} placeholder=' '></input>
+              <label className='form-element' htmlFor='passwordRepeat' >Repeat Password</label>
+            </>
+          )
+          : <></>
+      }
     </>
   );
 }
--- authentication-client/src/features/sign-up/SignUp.tsx
+++ authentication-client/src/features/sign-up/SignUp.tsx
@@ -1,10 +1,20 @@
 import { Link } from 'react-router-dom';
+import { Button } from '../../components/Button';
+import { Email } from '../../components/Email';
+import { Password } from '../../components/Password';
 
 export function SignUp() {
   return (
     <>
       <h1>Sign Up</h1>
-      <Link to='/sign-in'>Sign In</Link>
+      <form className='form'>
+        <Email />
+        <Password repeat={true} />
+        <Button text='Create Account' />
+        <div className='links-tupple'>
+          <Link className='underline' to='/sign-in'>I have an account</Link>
+        </div>
+      </form>
     </>
   );
 }
